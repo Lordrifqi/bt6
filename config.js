c17e12d3ed21e47e82771a05e9e91f26fdde18e3
Lordrifqi <94451407+Lordrifqi@users.noreply.github.com>
2022-07-16 17:00:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 let fs = require('fs')
-global.owner = JSON.parse(fs.readFileSync('./src/owner.json')) // Put your number to folder /src/owner.json
+global.owner = JSON.parse(fs.readFileSync('.6283104818812/src/owner.json')) // Put your number to folder /src/owner.json
 global.mods = JSON.parse(fs.readFileSync('./src/moderator.json')) // Want some help?
 
 global.APIs = { // API Prefix
@@ -37,11 +37,11 @@ global.eror = '_*𝒎𝒂𝒂𝒇 𝒔𝒆𝒓𝒗𝒆𝒓 𝒆𝒓𝒓𝒐𝒓.
 
 //========Url Template Buttons=========//
 global.dtu = '𝑌𝑜𝑢𝑡𝑢𝑏𝑒'
-global.urlnya = "https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ"
+global.urlnya = "https://www.youtube.com/channel/UCRqofdK0H_KTvCP1V7WLJpg/about"
 
 //============= callButtons =============//
 global.dtc = '𝒄𝒂𝒍𝒍 𝒐𝒘𝒏𝒆𝒓'
-global.phn = '62 857-1313-7903'
+global.phn = '6283104818812'
 
 //============= Games ================//
 global.benar = '_*Benar*_'
